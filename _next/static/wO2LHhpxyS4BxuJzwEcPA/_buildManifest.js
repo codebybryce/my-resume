a4169ef917f5dfa56e271dec9a8000a1f9e794ee
Bryce Robinson <74065233+codebybryce@users.noreply.github.com>
2022-06-02 14:30:30 +0000

    main Branch Fix

--- _next/static/wO2LHhpxyS4BxuJzwEcPA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-1688c4dbe4c7a872.js","static/chunks/pages/index-a6eef0bf95cf7e25.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
